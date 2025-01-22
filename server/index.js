2042c8b5ab1bb085bb46a87a872f42a367be88d7
Eunum56 <msiaffu123@gmail.com>
2025-01-22 14:48:49 +0530

    update timeout of connectiong mongoDB

--- server/index.js
+++ server/index.js
@@ -23,7 +23,8 @@ const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
-            useUnifiedTopology: true
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 50000
         });
         console.log("Connected to MongoDB");
     } catch (error) {
